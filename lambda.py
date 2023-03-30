c2ab4c81fd77d585acb6e52da6fa1325862e7122
vverrty <115580347+vverrty@users.noreply.github.com>
2023-03-31 00:55:53 +0300

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -19,7 +19,7 @@ def update():
         ExpressionAttributeNames = { "#ct": "counter" },
         ReturnValues = "UPDATED_NEW",
     )
-    #hello world asdasdadsads feeetete123test
+    #hello world asdasdadsads feeetete123test1231321
 def put14():
     table.put_item(
        Item={
